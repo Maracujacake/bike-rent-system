b3807e4e4f1c458ac2771da02b67d5379d3ebf68
Maracujacake <madlordx@gmail.com>
2024-06-26 19:36:29 -0300

    feat(locacao): DAO, Domain, Controller faltando coisa

--- bikes-rent/src/main/java/controllers/ControllerLocadora.java
+++ bikes-rent/src/main/java/controllers/ControllerLocadora.java
@@ -173,7 +173,8 @@ public class ControllerLocadora extends HttpServlet {
                 else {
                     String errorMessage = URLEncoder.encode("Locadora não encontrada. ID não reconhecido",
                             StandardCharsets.UTF_8.toString());
-                    response.sendRedirect(request.getContextPath() + "/locadoraView/buscaLocadora.jsp?error=" + errorMessage);
+                    response.sendRedirect(
+                            request.getContextPath() + "/locadoraView/buscaLocadora.jsp?error=" + errorMessage);
                 }
             }
 
@@ -196,17 +197,15 @@ public class ControllerLocadora extends HttpServlet {
         String Cidade = request.getParameter("cidade");
         try {
             List<Locadora> Locadoras = dao.getByCidade(Cidade);
-                    request.setAttribute("listaLocadorasCidade", Locadoras);
-                    RequestDispatcher dispatcher = request.getRequestDispatcher("/locadoraView/locadoraCidadeLista.jsp");
-                    dispatcher.forward(request, response);
-            }
-
-            catch (RuntimeException | IOException | ServletException e) {
-                throw new ServletException(e);
-            }
+            request.setAttribute("listaLocadorasCidade", Locadoras);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/locadoraView/locadoraCidadeLista.jsp");
+            dispatcher.forward(request, response);
         }
 
-
+        catch (RuntimeException | IOException | ServletException e) {
+            throw new ServletException(e);
+        }
+    }
 
     // Funções de UPDATE
 
